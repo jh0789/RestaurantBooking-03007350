acf2c2d3b5ba3b7632fa3a50772349304247ad5d
Janghee.Lee <lee.janghee@gmail.com>
2025-08-01 13:57:40 +0900

    [feature] 44

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -15,6 +15,14 @@ CUSTOMER_WITH_MAIL = Customer("Fake name", "010-1234-5678", "test@test.com")
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
+class TestableBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, data_time: str):
+        super().__init__(capacity_per_hour)
+        self._date_time = data_time
+
+    def get_now(self):
+        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
+
 class SunndayBookingScheduler(BookingScheduler):
     def __init__(self, capacity_per_hour):
         super().__init__(capacity_per_hour)
@@ -125,7 +133,8 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
     #arrange
-    booking_scheduler = SunndayBookingScheduler(CAPACITY_PER_HOUR)
+    #booking_scheduler = SunndayBookingScheduler(CAPACITY_PER_HOUR)
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2021/03/28 17:00")
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
     #act and assert
@@ -134,7 +143,8 @@ def test_현재날짜가_일요일인_경우_예약불가_예외처리():
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
     #arrange
-    booking_scheduler = MonndayBookingScheduler(CAPACITY_PER_HOUR)
+    #booking_scheduler = MonndayBookingScheduler(CAPACITY_PER_HOUR)
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2024/06/03 17:00")
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
     #act
