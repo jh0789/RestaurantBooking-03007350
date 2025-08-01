421cebd935ce26887e85caa4abec1fee8f7e2b4e
Janghee.Lee <lee.janghee@gmail.com>
2025-08-01 12:56:05 +0900

    [feature] 21

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -3,24 +3,33 @@ import pytest
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
-
 from datetime import datetime
-def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
+
+NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+CUSTOMER = Customer("Fake name", "010-1234-5678")
+
+UNDER_CAPACITY = 1
+CAPACITY_PER_HOUR = 3
+
+@pytest.fixture
+def booking_scheduler():
+    return BookingScheduler(CAPACITY_PER_HOUR)
+
+
+def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
     # arrange
-    not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
-    customer = Customer("Fake name", "010-1234-5678")
-    schedule = Schedule(not_on_the_hour, 1, customer)
-    booking_scheduler = BookingScheduler(3)
+    schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
     # act and assert
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
-def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
+
+def test_예약은_정시에만_가능하다_정시인_경우_예약가능(booking_scheduler):
     # arrange
-    on_the_hour = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
-    customer = Customer("Fake name", "010-1234-5678")
-    schedule = Schedule(on_the_hour, 1, customer)
-    booking_scheduler = BookingScheduler(3)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
     # act
     booking_scheduler.add_schedule(schedule)
     # assert
